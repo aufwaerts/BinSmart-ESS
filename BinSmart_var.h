72354c152a9e9ae0d1a8a3911796653b0ecd7b61
aufwaerts <binderoliver@ymail.com>
2025-02-18 09:22:59 +0100

    Add files via upload

---
+++ BinSmart_var.h
@@ -0,0 +1,63 @@
+// Global variables
+
+// Networking
+AsyncWebServer OTAserver(80);
+WiFiServer server(TELNET_PORT);
+WiFiClient telnet;
+HTTPClient http;
+String http_resp;
+String public_IP = "000.000.000.000", DDNS_address = "000.000.000.000";
+
+// BMS
+byte BMS_resp[300];
+int BMS_balancer_start;  // BMS balancer cell voltage threshold [mV] (read from BMS)
+int BMS_balancer_trigger;  // BMS balancer cell diff threshold [mV] (read from BMS)
+
+// Errors
+int error_counter[ERROR_TYPES];
+unsigned long errortime[ERROR_TYPES];
+int errors_consecutive = 0;
+String error_msg;  // if error(s) occured during polling cycle, type of error is written in here
+bool error_flag = false;  // errors occured and not yet read by user?
+
+// Hoymiles/RF24
+byte hm_radio_ID[5] = {0x01, 0xFF, 0xFF, 0xFF, 0xFF};  // 0xFF will be replaced by HM serial number
+// in the following HM commands, 0xFF will be replaced by HM serial number, 0x00 will be replaced by calculation
+byte hm_turnon[15] =  {0x51, 0xFF, 0xFF, 0xFF, 0xFF, 0x80, 0x17, 0x41, 0x72, 0x81, 0x00, 0x00, 0xB0, 0x01, 0x00};
+byte hm_turnoff[15] = {0x51, 0xFF, 0xFF, 0xFF, 0xFF, 0x80, 0x17, 0x41, 0x72, 0x81, 0x01, 0x00, 0x20, 0x00, 0x00};
+byte hm_power[19] =   {0x51, 0xFF, 0xFF, 0xFF, 0xFF, 0x80, 0x17, 0x41, 0x72, 0x81, 0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+RF24 radio(RF24_CE_PIN, RF24_CSN_PIN);
+CRC8 crc8;
+CRC16 crc16;
+
+// Time variables
+Dusk2Dawn ess_location(0,0,0);  // latitude, longitude and timezone will be read from Shelly 3EM
+String current_time = "00:00", sunrise = "00:00", sunset = "00:00";  // will be read from Shelly 3EM or replaced by calculation
+bool dst, daytime;  // flags for daylight saving time (DST) and daytime/nighttime
+String tz_identifier;  // ESS timezone as text (will be read from Shelly 3EM)
+unsigned long unixtime = 0, starttime = 0, resettime_errors = 0, resettime_energy = 0; // epoch times (will be read from Shelly 3EM)
+unsigned long minpower_time = 0;  // unixtime of last lowest power consumption reading
+unsigned long pubip_time = 0, DDNS_time = 0;  // unixtime of last public IP address check and last DDNS update
+unsigned long ts_cycle = 0, ts_power = 0, ts_HM_ON = 0, ts_MW_ON = 0, ts_pubip = 0, ts_EM_reset = 0;  // various timestamps
+
+// Other global variables
+int power_grid = 0, power_grid_min = 10000, power_pv = 0, power_ess = 0, power_new = 0, power_manual = 0;
+int power_target = POWER_TARGET_DEFAULT;
+int filter_cycles = POWER_FILTER_CYCLES;  // for filtering out power spikes
+int vcell_min, vcell_max;  // Cell min/max voltages (in millivolts)
+int bms_uvp;  // BMS Cell UVP value (read from BMS)
+long vbat;  // Total batt voltage [mV]
+int cbat;  // Batt charging/discharging current [cA]
+float pbat;  // Batt DC power [W]
+int mw_max_power;  // MW max charging power depends on vbat, will be calculated after first read of vbat from BMS
+int hm_power_limit = HM_MAX_POWER, mw_power_limit = mw_max_power;
+unsigned long mw_relay_counter;  // counter for Shelly MW plug relay ops
+bool mw_relay_off; // state of Shelly MW plug relay
+int uvp_countdown = UVP_WAKEUP_RESET;  // countdown before ESS falls asleep
+float from_pv = 0, pv_to_cons = 0, pv_to_ess = 0, pv_to_grid = 0, pv_consumed;  // PV energy counters [Wh]
+float from_grid = 0, to_grid = 0, grid_to_cons = 0, grid_to_ess = 0;  // Grid energy counters [Wh]
+float from_ess = 0, to_ess = 0, ess_to_cons = 0, ess_to_grid = 0;  // ESS energy counters [Wh]
+bool manual_mode = false, auto_recharge = false, pm_eco_mode = true;
+char buf[30];  // buffer for formatting output with sprintf()
+String cycle_msg, ota_msg, cmd_resp;
+char repeat_command = 0;  // saves user commands that are to be repeated
