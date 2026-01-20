ff4bec3d4f3212ba879b8f7b2c4ed8d03ecbdb26
aufwaerts <binderoliver@ymail.com>
2026-01-20 21:55:31 +0100

    Update BinSmart.ino

--- BinSmart.ino
+++ BinSmart.ino
@@ -146,7 +146,6 @@ void setup() {
     else telnet.println("Shelly 2PM found"); 
     if (!ShellyCommand(PM2_ADDR, PM_CH1_ON)) telnet.println(ERROR_SYMBOL + error_msg);
     if (!ShellyCommand(PM2_ADDR, PM_ECO_ON)) telnet.println(ERROR_SYMBOL + error_msg);
-    pm2_eco_mode = true;
 
     // Shelly 3EM: Erase energy data, read grid power and current time
     if (!ShellyCommand(EM_ADDR, EM_RESET)) telnet.println(ERROR_SYMBOL + error_msg);
